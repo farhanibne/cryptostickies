1ac1333c6e881e70b89bd9a2515bad8dc78ddcdf
farhan ibne saif <farhanibne860@gmail.com>
2022-08-03 06:10:20 -0400

    Add files via upload

---
+++ src/components/header/Buy.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { HiCurrencyDollar} from 'react-icons/hi';
+
+function Buy() {
+  return (
+    
+    <>
+    
+    
+        <a href="https://opensea.io/collection/cryptostickies-100" target={'_blank'}>
+            <HiCurrencyDollar/>
+        </a>
+
+    
+
+    </>
+  )
+}
+
+export default Buy
